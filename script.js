6d9c0dbf7fba5d64449b198fd7507cfa5cb2212f
Jarad Overby <joverby@css.edu>
2023-08-17 16:21:47 -0500

    Got initial calculator layout, next step is to add logic for when buttons are pressed and do corresponding operation

---
+++ script.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const calc = document.querySelector(".calc");
+const display = document.querySelector(".calc-display");
+
+calc.addEventListener("click", function (e) {
+  console.log(e);
+
+  if (e.target.localName === "button") {
+    console.log("Button Clicked");
+    console.log(e.target.innerText);
+    display.textContent = e.target.innerText;
+  } else {
+    console.log("Div Clicked");
+  }
+});
+
+const add = function (a, b) {};
+
+const subtract = function (a, b) {};
+
+const multiply = function (a, b) {};
+
+const divide = function (a, b) {};
